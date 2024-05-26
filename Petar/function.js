32fb2fdd57915f257a92f9549102b4a2763f4786
PetarS09 <162906881+PetarS09@users.noreply.github.com>
2024-05-26 18:08:49 +0300

    Done with project.Made the calculator function in boxing.

--- Petar/function.js
+++ Petar/function.js
@@ -1,3 +1,51 @@
+//Display group
+function findCategory() {
+    const weight = parseFloat(document.getElementById('weight').value);
+    let category = '';
+
+    if (isNaN(weight)) {
+        category = 'Please enter a valid number.';
+    } else if (weight < 48) {
+        category = 'Below minimumweight to be in a division.';
+    } else if (weight <= 48) {
+        category = ' You are in minimumweight division.';
+    } else if (weight <= 51) {
+        category = 'You are in Light Flyweight division.';
+    } else if (weight <= 52) {
+        category = ' You are in Flyweight division.';
+    } else if (weight <= 55) {
+        category = ' You are in Super Flyweight division.';
+    } else if (weight <= 57) {
+        category = 'You are in Bantamweight division.';
+    } else if (weight <= 59) {
+        category = 'You are in Super Bantamweight division.';
+    } else if (weight <= 61) {
+        category = 'You are in Featherweight division.';
+    } else if (weight <= 64) {
+        category = 'You are in Super Featherweight division.';
+    } else if (weight <= 66) {
+        category = 'You are in Lightweight division.';
+    } else if (weight <= 69) {
+        category = 'You are in Super Lightweight division.';
+    } else if (weight <= 72) {
+        category = 'You are in Welterweight division.';
+    } else if (weight <= 76) {
+        category = 'You are in Super Welterweight division.';
+    } else if (weight <= 79) {
+        category = 'You are in Middleweight division.';
+    } else if (weight <= 81) {
+        category = 'You are in Super Middleweight division.';
+    } else if (weight <= 86) {
+        category = 'You are in Light Heavyweight division.';
+    } else if (weight <= 91) {
+        category = 'You are in Cruiserweight division.';
+    } else {
+        category = 'You are in Heavyweight division.';
+    }
+
+    document.getElementById('category').innerText = category;
+}
+//Display group
 function Change_Correct(){
     document.getElementById("Correct").innerHTML="This answer is correct!!!";
     document.getElementById("Correct").style.color="green";
@@ -49,7 +97,7 @@ function Change_wrong7(){
     document.getElementById("Wrong7").innerHTML="Wrong answer.Choose another.";
     document.getElementById("Wrong7").style.color="red";
 }
-//Fourth Question
+//fourth question
 function Change_wrong8(){
     document.getElementById("Wrong8").innerHTML="Wrong answer.Choose another.";
     document.getElementById("Wrong8").style.color="red";
@@ -76,3 +124,16 @@ function Change_Correct5(){
     document.getElementById("Correct5").style.color="green";
 }
 //sixth question
+function Change_wrong12(){
+    document.getElementById("Wrong12").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong12").style.color="red";
+}
+function Change_Correct6(){
+    document.getElementById("Correct6").innerHTML="This answer is correct!!!";
+    document.getElementById("Correct6").style.color="green";
+}
+function Change_wrong13(){
+    document.getElementById("Wrong13").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong13").style.color="red";
+}
+//seventh question
