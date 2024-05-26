1248a0d7851a81a97f790a4022ac7759e2ad5831
venev09 <valerikirilov13@gmail.com>
2024-05-26 19:46:50 +0300

    chore: Update CSS styles for quiz page and add quiz functionality

---
+++ Valeri/script.js
@@ -0,0 +1,152 @@
+let userAns=[];
+    let correctAns = [2, 4, 3];
+    let result1=" ";
+    let result2=" ";
+    let result3=" ";
+
+
+
+document.getElementById("choise1.1").addEventListener("click", function() {
+    userAns[0] = 1;
+    console.log(userAns[0]);
+});
+
+document.getElementById("choise1.2").addEventListener("click", function() {
+    userAns[0] = 2;
+    console.log(userAns[0]);
+});
+
+document.getElementById("choise1.3").addEventListener("click", function() {
+    userAns[0] = 3;
+    console.log(userAns[0]);
+});
+
+
+document.getElementById("choise1.4").addEventListener("click", function() {
+    userAns[0] = 4;
+    console.log(userAns[0]);
+});
+
+
+document.getElementById("choise2.1").addEventListener("click", function() {
+    userAns[1] = 1;
+    console.log(userAns[1]);
+});
+
+
+document.getElementById("choise2.2").addEventListener("click", function() {
+    userAns[1] = 2;
+    console.log(userAns[1]);
+});
+
+document.getElementById("choise2.3").addEventListener("click", function() {
+    userAns[1] = 3;
+    console.log(userAns[1]);
+});
+
+
+document.getElementById("choise2.4").addEventListener("click", function() {
+    userAns[1] = 4;
+    console.log(userAns[1]);
+});
+
+
+document.getElementById("choise3.1").addEventListener("click", function() {
+    userAns[2] = 1;
+    console.log(userAns[2]);
+});
+
+document.getElementById("choise3.2").addEventListener("click", function() {
+    userAns[2] = 2;
+    console.log(userAns[2]);
+});
+
+document.getElementById("choise3.3").addEventListener("click", function() {
+    userAns[2] =3;
+    console.log(userAns[2]);
+});
+
+document.getElementById("choise3.4").addEventListener("click", function() {
+    userAns[2] = 4;
+    console.log(userAns[2]);
+});
+
+
+
+
+
+document.getElementById("submit").addEventListener("click", function() {
+    if (userAns[0] === correctAns[0]) {
+        result1 = "Answer 1 is Correct";
+    } else {
+        result1 = "Answer 1 is Incorrect";
+    }
+
+    if (userAns[1] === correctAns[1]) {
+        result2 = "Answer 2 is Correct";
+    
+    }
+    else {
+        result2 = "Answer 2 is Incorrect";
+    }
+
+    if (userAns[2] === correctAns[2]) {
+        result3 = "Answer 3 is Correct";
+    }
+    else {
+        result3 = "Answer 3 is Incorrect";
+    }
+
+    
+    console.log(userAns);
+    console.log(correctAns);
+    document.getElementById("result1").innerHTML = result1;
+    document.getElementById("result2").innerHTML = result2;
+    document.getElementById("result3").innerHTML = result3;
+});
+
+
+/*
+for (let i = 1; i <= 2; i++) {
+    for (let j = 0; j < 4; j++) {
+        document.getElementById("choise" + i + "." + j).addEventListener("click", function() {
+            userAns[i-1] = j;
+            console.log(userAns[i-1]);
+        });
+    }
+}
+
+
+document.getElementById("submit").addEventListener("click", function() {
+    for (let i = 0; i < 3; i++) {
+        if (userAns[i] == correctAns[i]) {
+            document.getElementById("result" + i).innerHTML = "Correct";
+        } else {
+            document.getElementById("result" + i).innerHTML = "Incorrect";
+        }
+    }
+
+
+});
+*/
+/*
+document.getElementById("choise1.1").addEventListener("click", function() {
+  userAns1 = 2;
+  console.log(userAns1);
+});
+
+document.getElementById("choise1.2").addEventListener("click", function() {
+    userAns1 = 2;
+    console.log(userAns1);
+});
+
+document.getElementById("choise1.3").addEventListener("click", function() {
+    userAns1 = 3;
+    console.log(userAns1);
+  });
+
+document.getElementById("choise1.4").addEventListener("click", function() {
+    userAns1 = 4;
+    console.log(userAns1);
+});
+*/
