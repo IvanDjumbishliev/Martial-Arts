8f8d2d6cba1530b55de2360a95fe668ab04e5c13
IvanDjumbishliev <ivan.n.dzhumbishliev.2023@elsys-bg.org>
2024-05-26 18:34:23 +0300

    chore: Add Muay Thai techniques to script.js

---
+++ Ivan/script.js
@@ -0,0 +1,23 @@
+function punches(){
+    document.getElementById("punch").innerHTML = "Punches are the most commonly used weapons in Muay Thai. The main punch techniques include the straight lead punch (jab), straight rear punch (cross), uppercut, hook, overhead punch, and spinning back fist. Punching power is generated with speed from the feet up, through shifting of weight, and rotation of the hip and shoulders.";
+}
+
+function elbow(){
+    document.getElementById("elbow").innerHTML = "The elbow is one of the hardest parts of the human body, making them an effective weapon in Muay Thai. There are many different ways to throw an elbow: sideways to the head, from the top down, reverse to the chin, flying elbow from the top down and spinning back elbow. When executed properly, elbow strikes can knock out an opponent or inflict deep cuts that can end fights via stoppages.";
+}
+
+function kick(){
+    document.getElementById("kick").innerHTML = "The Muay Thai kick is a devastating weapon delivered via the shins. The kick is delivered from the outside, with the arm swung backwards along with the inside rotation of the hip to generate force. Kicks can be thrown to the opponent’s legs (known as low kick or leg kick), body, arms, the back or the head. ";
+}
+
+function knee(){
+    document.getElementById("knee").innerHTML = "Knees in Muay Thai are typically close range weapons used during clinching. They are often thrown to the body, especially the ribcage but also on thighs and directly to the head. Jumping knee strikes can also be devastating if landed. When used effectively like other weapons of Muay Thai, knees can end fights by knockouts.";
+}
+
+function teep(){
+    document.getElementById("teep").innerHTML = "The push kick, referred to as “teep” in Muay Thai, can be used as both defensively or offensively. It can be used as a technique to keep opponents at a distance and disrupting an advance or as a strike if delivered with power and accuracy.";
+}
+
+function clinch(){
+    document.getElementById("clinch").innerHTML = "Clinching in Muay Thai is a grappling technique usually used in combination with knee and elbow strikes. Clinching is a close range fighting style that requires many years of practice to master. Takedowns, or tripping the opponent to the ground are also allowed and performed during clinching. When used to the right effect, clinching can help fighters outscore their opponents and win fights.";
+}
