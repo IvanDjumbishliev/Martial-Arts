d11eb0fad49b40fbca706709e4dadd3377562f70
PetarS09 <162906881+PetarS09@users.noreply.github.com>
2024-05-26 12:52:19 +0300

    Update function.js

---
+++ Petar/function.js
@@ -0,0 +1,78 @@
+function Change_Correct(){
+    document.getElementById("Correct").innerHTML="This answer is correct!!!";
+    document.getElementById("Correct").style.color="green";
+}
+function Change_wrong(){
+    document.getElementById("Wrong").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong").style.color="red";
+}
+function Change_wrong1(){
+    document.getElementById("Wrong1").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong1").style.color="red";
+}
+//First question
+function Change_wrong2(){
+    document.getElementById("Wrong2").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong2").style.color="red";
+}
+function Change_wrong3(){
+    document.getElementById("Wrong3").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong3").style.color="red";
+}
+function Change_Correct1(){
+    document.getElementById("Correct1").innerHTML="This answer is correct!!!";
+    document.getElementById("Correct1").style.color="green";
+}
+//second question
+function Change_wrong4(){
+    document.getElementById("Wrong4").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong4").style.color="red";
+}
+function Change_Correct2(){
+    document.getElementById("Correct2").innerHTML="This answer is correct!!!";
+    document.getElementById("Correct2").style.color="green";
+}
+function Change_wrong5(){
+    document.getElementById("Wrong5").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong5").style.color="red";
+}
+//third question
+function Change_Correct3(){
+    document.getElementById("Correct3").innerHTML="This answer is correct!!!";
+    document.getElementById("Correct3").style.color="green";
+}
+function Change_wrong6(){
+    document.getElementById("Wrong6").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong6").style.color="red";
+}
+function Change_wrong7(){
+    document.getElementById("Wrong7").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong7").style.color="red";
+}
+//Fourth Question
+function Change_wrong8(){
+    document.getElementById("Wrong8").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong8").style.color="red";
+}
+function Change_Correct4(){
+    document.getElementById("Correct4").innerHTML="This answer is correct!!!";
+    document.getElementById("Correct4").style.color="green";
+}
+function Change_wrong9(){
+    document.getElementById("Wrong9").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong9").style.color="red";
+}
+//fifthy question
+function Change_wrong10(){
+    document.getElementById("Wrong10").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong10").style.color="red";
+}
+function Change_wrong11(){
+    document.getElementById("Wrong11").innerHTML="Wrong answer.Choose another.";
+    document.getElementById("Wrong11").style.color="red";
+}
+function Change_Correct5(){
+    document.getElementById("Correct5").innerHTML="This answer is correct!!!";
+    document.getElementById("Correct5").style.color="green";
+}
+//sixth question
